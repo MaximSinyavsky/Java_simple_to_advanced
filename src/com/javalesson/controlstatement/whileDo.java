df36efb57b653ab296f9ad85a9e7c6e455eb23e7
Razarsinet1991@gmail.com <razarsinet1991@gmail.com>
2022-06-03 14:32:36 +0600

    update whileDo

--- src/com/javalesson/controlstatement/whileDo.java
+++ src/com/javalesson/controlstatement/whileDo.java
@@ -2,6 +2,23 @@ package com.javalesson.controlstatement;
 
 public class whileDo {
     public static void main(String[] args) {
-        
+        int finalBalance = 100_000;
+        double currentBalace = 0;
+        int payment = 10_00;
+        int years = 0;
+        double interestRate = 0.1;
+//        while (currentBalace < finalBalance) {
+//            currentBalace += payment;
+//            currentBalace = currentBalace + currentBalace * interestRate;
+//            years++;
+//            System.out.println("Year " + years + " " + currentBalace);
+//        }
+        do {
+            currentBalace += payment;
+            currentBalace = currentBalace + currentBalace * interestRate;
+            years++;
+            System.out.println("Year " + years + " " + currentBalace);
+        } while (currentBalace < finalBalance && years < 15);
+
     }
 }
