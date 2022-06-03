adfc49ce99d25b20e8ad7ed56ac49958763dbdd6
Razarsinet1991@gmail.com <razarsinet1991@gmail.com>
2022-06-03 15:09:56 +0600

    update whileDo

---
+++ src/com/javalesson/controlstatement/switchCase.java
@@ -0,0 +1,32 @@
+package com.javalesson.controlstatement;
+
+public class switchCase {
+    public static void main(String[] args) {
+        String dayOfTheWeek = monday;
+        switch (dayOfTheWeek) {
+            case "monday":
+                System.out.println("this is first working day");
+                break;
+            case "monday":
+                System.out.println("this is second working day");
+                break;
+            case "tuesday":
+                System.out.println("this is thirst working day");
+                break;
+            case "monday":
+                System.out.println("this is first working day");
+                break;
+            case "monday":
+                System.out.println("this is first working day");
+                break;
+            case "monday":
+                System.out.println("this is first working day");
+                break;
+            case "monday":
+                System.out.println("this is first working day");
+                break;
+            default:
+                System.out.println("unsupported options");
+        }
+    }
+}
