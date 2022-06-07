2e96cca7f728a51d8180926c34818c1a22aab3cb
Razarsinet1991@gmail.com <razarsinet1991@gmail.com>
2022-06-07 15:52:53 +0600

    update switchCase

--- src/com/javalesson/controlstatement/switchCase.java
+++ src/com/javalesson/controlstatement/switchCase.java
@@ -2,31 +2,28 @@ package com.javalesson.controlstatement;
 
 public class switchCase {
     public static void main(String[] args) {
-        String dayOfTheWeek = monday;
-        switch (dayOfTheWeek) {
+        String dayOfTheWeek = args[0];
+        switch (dayOfTheWeek.toLowerCase()) {
             case "monday":
                 System.out.println("this is first working day");
-                break;
-            case "monday":
-                System.out.println("this is second working day");
                 break;
             case "tuesday":
+                System.out.println("this is second working day");
+                break;
+            case "wednesday":
                 System.out.println("this is thirst working day");
                 break;
-            case "monday":
-                System.out.println("this is first working day");
+            case "thursday":
+                System.out.println("this is fourth working day");
                 break;
-            case "monday":
-                System.out.println("this is first working day");
+            case "friday":
+                System.out.println("this is fifth working day");
                 break;
-            case "monday":
-                System.out.println("this is first working day");
-                break;
-            case "monday":
-                System.out.println("this is first working day");
+            case "saturday": case "sunday":
+                System.out.println("finally weekend");
                 break;
             default:
-                System.out.println("unsupported options");
+                System.out.println("not a day of the week");
         }
     }
 }
