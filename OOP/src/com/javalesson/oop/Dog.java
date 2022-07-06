2761070c44beb5b623e0280e2472dce05a781081
Razarsinet1991@gmail.com <razarsinet1991@gmail.com>
2022-07-06 15:11:29 +0600

    update

--- OOP/src/com/javalesson/oop/Dog.java
+++ OOP/src/com/javalesson/oop/Dog.java
@@ -1,12 +1,39 @@
 package com.javalesson.oop;
 
 public class Dog {
+    private static int dogsCount;
+
     private int paws;
     private int tail;
     private String name;
     private String breed;
+    private String size;
+
+    public Dog() {
+        dogsCount++;
+        System.out.println("Dog's count is " + dogsCount);
+    }
+
+    public static int getDogsCount() {
+        return dogsCount;
+    }
 
     // STOPSHIP: 06.07.2022
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        if (size.equalsIgnoreCase("big") ||
+                size.equalsIgnoreCase("average") ||
+                size.equalsIgnoreCase("small")) {
+            this.size = size;
+        } else {
+            System.out.println("Size should be one of these: Big, Average or Small");
+        }
+
+    }
 
     public void setName(String name) {
         this.name = name;
@@ -51,5 +78,24 @@ public class Dog {
     public void setBreed(String breed) {
         this.breed = breed;
     }
+
     // STOPSHIP: 06.07.2022 lesson 16;
+    public void bark() {
+        if (size.equalsIgnoreCase("Big")) {
+            System.out.println("Wof-Wof");
+        } else if (size.equalsIgnoreCase("Average")) {
+            System.out.println("Raf-Raf");
+        } else {
+            System.out.println("Tyf-Tyf");
+        }
+    }
+
+    public void bite() {
+        if (dogsCount > 2) {
+            System.out.println("Dogs are biting you");
+        } else {
+            bark();
+        }
+    }
 }
+// STOPSHIP: 06.07.2022 lesson 17; 
--- OOP/src/com/javalesson/oop/Main.java
+++ OOP/src/com/javalesson/oop/Main.java
@@ -2,6 +2,9 @@ package com.javalesson.oop;
 
 public class Main {
     public static void main(String[] args) {
+
+        System.out.println("Dog's count " + Dog.getDogsCount());
+
         Dog lab = new Dog();
 //        dog.paws = 4;
 //        dog.tail = 1;
@@ -12,16 +15,32 @@ public class Main {
         lab.setTail(1);
         lab.setName("Tyson");
         lab.setBreed("lab");
+        lab.setSize("Average");
+        //lab.bark();
+        lab.bite();
 
         Dog sheppard = new Dog();
-        sheppard.setPaws(5);
+        sheppard.setPaws(4);
         sheppard.setName("Rex");
         sheppard.setBreed("sheppard");
-        sheppard.setTail(2);
+        sheppard.setTail(1);
+        sheppard.setSize("BIG");
+        //sheppard.bark();
+        sheppard.bite();
+
+        Dog doberman = new Dog();
+        doberman.setSize("Big");
+        doberman.setName("Baddy");
+        doberman.setTail(1);
+        doberman.setPaws(4);
+        doberman.setBreed("doberman");
+        doberman.bite();
 
         System.out.println("Lab's name is " + lab.getName());
         System.out.println("Sheppard's name is " + sheppard.getName());
         System.out.println("Sheppard's " + sheppard.getPaws() + " paws");
         // STOPSHIP: 06.07.2022 lesson 16;
+
     }
 }
+// STOPSHIP: 06.07.2022 lesson 17;
