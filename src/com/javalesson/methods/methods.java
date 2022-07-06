243d18895291cdda5bfeca1945ae03d8946e8efd
Razarsinet1991@gmail.com <razarsinet1991@gmail.com>
2022-07-06 11:43:57 +0600

    update methods

--- src/com/javalesson/methods/methods.java
+++ src/com/javalesson/methods/methods.java
@@ -10,17 +10,24 @@ public class methods {
         //int sumOfSquares = calcSquare(b) +calcRectangleSquare(5, 10);
         //System.out.println("sum Of Squares = "+sumOfSquares);
         System.out.println("sum Of Squares = " + (calcSquare(10) + calcRectangleSquare(5, 10)));
-
-        String str1 = "i love coffee";
+        String str1 = "I like black coffee";
         System.out.println(str1.toUpperCase());
         System.out.println(str1.toLowerCase());
         // STOPSHIP: 23.06.2022
-        String str2 = "i love coffee";
-        String str3 = "i love coffee!!!";
-        str3.substring(0,13);
-        boolean b = str2 == str1;
-        System.out.println(b);
-        System.out.println(str3.substring(0,13));
+        String str2 = "I like coffee";
+        String str3 = "I like coffee!!!";
+        String substring = str3.substring(0, 13);
+        System.out.println("substring = " + substring);
+        String str4 = new String("i like coffee");
+        boolean b = str2 == str4;
+        //System.out.println(b);
+        boolean c = str4.equals(str4);
+        //System.out.println(c);
+        boolean like = str1.startsWith(" like", 1);
+        System.out.println(like);
+        boolean like2 = str2.endsWith("fee");
+        System.out.println(like2);
+        System.out.println(str1.replace("black","white"));
 
     }
 
