0d1bc99641d6c8c894069e6fb3507ab410f1b15b
Razarsinet1991@gmail.com <razarsinet1991@gmail.com>
2022-07-06 14:12:20 +0600

    update

--- OOP/src/com/javalesson/oop/Dog.java
+++ OOP/src/com/javalesson/oop/Dog.java
@@ -1,9 +1,55 @@
 package com.javalesson.oop;
 
 public class Dog {
-    private int paws = 4;
-    private int tail = 1;
+    private int paws;
+    private int tail;
     private String name;
     private String breed;
-    // STOPSHIP: 06.07.2022  
+
+    // STOPSHIP: 06.07.2022
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPaws() {
+        return paws;
+    }
+
+    public void setPaws(int paws) {
+        if (paws == 4) {
+            this.paws = paws;
+        } else {
+            this.paws = 4;
+            System.out.println("User tried to assign " + paws + " paws");
+            System.out.println("Correct number is four");
+        }
+    }
+
+    public int getTail() {
+        return tail;
+    }
+
+    public void setTail(int tail) {
+        if (tail == 1) {
+            this.tail = tail;
+        } else {
+            this.tail = 1;
+            System.out.println("User tried to assign " + tail + " tail");
+            System.out.println("Correct number is one");
+        }
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+    // STOPSHIP: 06.07.2022 lesson 16;
 }
--- OOP/src/com/javalesson/oop/Main.java
+++ OOP/src/com/javalesson/oop/Main.java
@@ -2,13 +2,26 @@ package com.javalesson.oop;
 
 public class Main {
     public static void main(String[] args) {
-        Dog dog = new Dog();
+        Dog lab = new Dog();
 //        dog.paws = 4;
 //        dog.tail = 1;
 //        dog.name = "Tyson";
 //        dog.breed = "lab";
-        // STOPSHIP: 06.07.2022  
+        // STOPSHIP: 06.07.2022
+        lab.setPaws(4);
+        lab.setTail(1);
+        lab.setName("Tyson");
+        lab.setBreed("lab");
 
+        Dog sheppard = new Dog();
+        sheppard.setPaws(5);
+        sheppard.setName("Rex");
+        sheppard.setBreed("sheppard");
+        sheppard.setTail(2);
 
+        System.out.println("Lab's name is " + lab.getName());
+        System.out.println("Sheppard's name is " + sheppard.getName());
+        System.out.println("Sheppard's " + sheppard.getPaws() + " paws");
+        // STOPSHIP: 06.07.2022 lesson 16;
     }
 }
